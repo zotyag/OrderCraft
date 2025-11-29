27fa7af07b859c83a7b287a1337c554ab24aceb8
Gombar_Zolta <gombarzolta@gmail.com>
2025-11-29 21:50:52 +0100

    feat: adds Backend/dto Auth and Register

---
+++ Backend/src/main/java/com/gombar/OrderCraft/dto/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.gombar.OrderCraft.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private Long userId;
+    private String username;
+    private String role;
+    private String email;
+}
---
+++ Backend/src/main/java/com/gombar/OrderCraft/dto/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.gombar.OrderCraft.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+}
