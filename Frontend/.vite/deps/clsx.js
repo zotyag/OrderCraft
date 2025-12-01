4aeee46e7fc9c691a1dbaa7f56deb26f17f9a0d2
Gombar_Zolta <gombarzolta@gmail.com>
2025-12-01 02:58:54 +0100

    chore: stopped tracking .vite folder

--- Frontend/.vite/deps/clsx.js
+++
@@ -1,9 +0,0 @@
-import {
-  clsx,
-  clsx_default
-} from "./chunk-KDVGFZWC.js";
-import "./chunk-G3PMV62Z.js";
-export {
-  clsx,
-  clsx_default as default
-};
--- Frontend/.vite/deps/react.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react
-} from "./chunk-ULSRCYB6.js";
-import "./chunk-G3PMV62Z.js";
-export default require_react();
